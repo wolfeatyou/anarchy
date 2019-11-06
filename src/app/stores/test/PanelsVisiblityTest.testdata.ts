3682aa82067c2c61cbfdd5658dc719d9894d2b90
wolfeatyou <fivefong@gmail.com>
2019-11-06 19:58:48 +0300

    new layout model

--- src/app/stores/test/PanelsVisiblityTest.testdata.ts
+++ src/app/stores/test/PanelsVisiblityTest.testdata.ts
@@ -14,70 +14,124 @@ export class PanelsVisiblityTestData {
   init() {
     this.appState = new ApplicationState();
     this.appState.metadataResolver.addMetadataPackage('test', this.testPackage);
-    this.appState.setActivePanel('test', 'officerGroups');
+    this.appState.setActivePanel('test', 'officerGroupDetails');
 
 
   }
 
 
   testPackage: any = {
-    officerGroups: {
-      code: 'officerGroups',
-      dataSources: [
+    officersAndGrantsPage: {
+      display: 'page',
+      props: {
+        title: 'Officers & Grants'
+      },
+      toolbar: {
+        items: []
+      },
+      sectionsTemplate:
+        'master details details' +
+        'master details details',
+      sectionsDetails: [
         {
-          code: 'officerGroupsDs',
-          operations: [
-            {
-              code: 'readOfficerGroups',
-              type: 'read'
-            }
-          ]
-        }
-      ],
-      tabs: [
-        {
-          code: 'officersTab',
-          linkedPanelCode: 'officers'
+          code: 'master',
+          linkedPanelCode: 'officerGroupsList',
         },
         {
-          code: 'roles',
-          linkedPanelCode: 'roles'
+          code: 'details',
+          linkedPanelCode: 'officerGroupDetails',
+        }
+      ]
+    }
+
+    ,
+    officerGroupsList: {
+      code: 'officerGroupsList',
+      display: 'list',
+      sharedProperties:
+        [
+          {
+            title: 'Officer Groups'
+          }
+        ]
+      ,
+      links: [
+        {
+          code: 'Details',
+          linkedPanelCode: 'officerGroupDetails',
+          linkedPanelSection: 'details'
         }
       ]
     },
 
+    officerGroupDetails: {
+      code: 'officerGroupDetails',
+      dataSources:
+        [
+          {
+            code: 'officerGroupsDs',
+            operations: [
+              {
+                code: 'readOfficerGroups',
+                type: 'read'
+              }
+            ]
+          }
+        ]
+      ,
+      tabs:
+        [
+          {
+            code: 'officersTab',
+            linkedPanelCode: 'officers'
+          }
+          , {
+          code: 'roles'
+          ,
+          linkedPanelCode: 'roles'
+        }
+
+        ]
+
+    }
+
+    ,
+
     roles: {
       code: 'roles',
-      dataSources: [
-        {
-          code: 'rolesDs',
-          operations: [
-            {
-              code: 'readRoles',
-              type: 'read'
-            }
-          ]
-        }
-      ],
+      dataSources:
+        [
+          {
+            code: 'rolesDs',
+            operations: [
+              {
+                code: 'readRoles',
+                type: 'read'
+              }
+            ]
+          }
+        ],
       tabs: []
-    },
+    }
+
+    ,
 
     officers: {
       code: 'officers',
-      dataSources: [
-        {
-          code: 'officersDs',
-          operations: [
-            {
-              code: 'readOfficers',
-              type: 'read'
-            }
-          ]
-        }
-      ],
+      dataSources:
+        [
+          {
+            code: 'officersDs',
+            operations: [
+              {
+                code: 'readOfficers',
+                type: 'read'
+              }
+            ]
+          }
+        ],
       tabs: []
     }
   };
-
 
 }
