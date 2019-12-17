d6d07035747be97a15bcea8fbbe4736369393f20
wolfeatyou <fivefong@gmail.com>
2019-12-18 00:00:42 +0300

    label

---
+++ src/app/components/label/label.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {BarState} from "../../stores/bar.state";
+import {PartState} from "../../stores/part.state";
+import {LabelState} from "../../stores/label.state";
+
+@Component({
+  selector: 'app-label',
+  templateUrl: './label.component.html',
+  styleUrls: ['./label.component.less']
+})
+export class LabelComponent implements OnInit {
+
+  private labelState: LabelState;
+  constructor() { }
+
+  @Input()
+  set state(label: LabelState) {
+    this.labelState = label;
+  }
+
+  get state() {
+    return this.labelState;
+  }
+
+  ngOnInit() {
+  }
+
+}
